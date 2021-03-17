11f1885e18f1412c6d58aa412254e5e684c82e82
pankajiet90 <pankajiet90@gmail.com>
2021-03-17 17:06:50 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login'
+import Regis from './Regis';
+
+function App() {
+  return (
+    <div className="App">
+     <Login />
+     <Regis />
+    </div>
+  );
+}
+
+export default App;
